6ebaaaac5d929c5b1eb849429a1ca8a145bb57b0
images if <eler30.mr@gmail.com>
2026-08-27 13:53:17 -0500

    feat(vocoder): add LYKENOX compact vocoder package

---
+++ lykenox_voice_engine/models/vocoder/__init__.py
@@ -0,0 +1,6 @@
+"""LYKENOX-owned neural vocoder components."""
+
+from .config import LykenoxVocoderConfig
+from .network import LykenoxVocoderGenerator
+
+__all__ = ["LykenoxVocoderConfig", "LykenoxVocoderGenerator"]
