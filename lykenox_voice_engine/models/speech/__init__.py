1244567fc2b7c377dea407b473210ffc1a78d1d8
images if <eler30.mr@gmail.com>
2026-08-27 10:27:15 -0500

    feat(speech): add LYKENOX-owned speech model package

---
+++ lykenox_voice_engine/models/speech/__init__.py
@@ -0,0 +1,6 @@
+"""LYKENOX-owned speech model components."""
+
+from .config import LykenoxSpeechConfig
+from .network import LykenoxSpeechAcousticModel
+
+__all__ = ["LykenoxSpeechConfig", "LykenoxSpeechAcousticModel"]
