e582714603c121e8eac0282d33705f3026db3f4e
images if <eler30.mr@gmail.com>
2026-08-27 14:08:45 -0500

    feat(vocoder): export training discriminator

--- lykenox_voice_engine/models/vocoder/__init__.py
+++ lykenox_voice_engine/models/vocoder/__init__.py
@@ -1,6 +1,17 @@
 """LYKENOX-owned neural vocoder components."""
 
 from .config import LykenoxVocoderConfig
+from .discriminator import (
+    DISCRIMINATOR_ARCHITECTURE,
+    DiscriminatorOutput,
+    LykenoxMultiScaleWaveformDiscriminator,
+)
 from .network import LykenoxVocoderGenerator
 
-__all__ = ["LykenoxVocoderConfig", "LykenoxVocoderGenerator"]
+__all__ = [
+    "DISCRIMINATOR_ARCHITECTURE",
+    "DiscriminatorOutput",
+    "LykenoxMultiScaleWaveformDiscriminator",
+    "LykenoxVocoderConfig",
+    "LykenoxVocoderGenerator",
+]
