c6edcb0df8170388a23bc6f11704611121a55573
images if <eler30.mr@gmail.com>
2026-08-28 12:40:47 -0500

    feat(runtime): export speech conditioning contract

--- lykenox_voice_engine/runtime/__init__.py
+++ lykenox_voice_engine/runtime/__init__.py
@@ -1,5 +1,23 @@
 """LYKENOX product-owned inference runtime contracts."""
 
 from .base import LykenoxRuntime, LykenoxSingingRuntime, LykenoxSpeechRuntime
+from .speech_conditioning import (
+    DEFAULT_VOICING_THRESHOLD,
+    PREDICTED_SPEECH_F0_MAX_HZ,
+    PREDICTED_SPEECH_F0_MIN_HZ,
+    SPEECH_VOCODER_CONDITIONING_VERSION,
+    SpeechVocoderConditioning,
+    prepare_speech_vocoder_conditioning,
+)
 
-__all__ = ["LykenoxRuntime", "LykenoxSpeechRuntime", "LykenoxSingingRuntime"]
+__all__ = [
+    "LykenoxRuntime",
+    "LykenoxSpeechRuntime",
+    "LykenoxSingingRuntime",
+    "SPEECH_VOCODER_CONDITIONING_VERSION",
+    "PREDICTED_SPEECH_F0_MIN_HZ",
+    "PREDICTED_SPEECH_F0_MAX_HZ",
+    "DEFAULT_VOICING_THRESHOLD",
+    "SpeechVocoderConditioning",
+    "prepare_speech_vocoder_conditioning",
+]
