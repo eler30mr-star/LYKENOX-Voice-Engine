b46d79c619fcc9935e00be2fe2fb97a56f489795
images if <eler30.mr@gmail.com>
2026-08-30 19:14:08 -0500

    feat(acoustic): export mel residual postnet candidate

--- lykenox_voice_engine/models/speech/__init__.py
+++ lykenox_voice_engine/models/speech/__init__.py
@@ -2,6 +2,11 @@
 
 from .alignment import LykenoxCTCAligner, LykenoxCTCAlignerConfig
 from .config import LykenoxSpeechConfig
+from .mel_postnet import (
+    MEL_POSTNET_ARCHITECTURE_V1,
+    LykenoxAcousticMelPostnetCandidate,
+    MelResidualPostnetV1,
+)
 from .network import LykenoxSpeechAcousticModel
 
 __all__ = [
@@ -9,4 +14,7 @@ __all__ = [
     "LykenoxCTCAlignerConfig",
     "LykenoxSpeechConfig",
     "LykenoxSpeechAcousticModel",
+    "MEL_POSTNET_ARCHITECTURE_V1",
+    "MelResidualPostnetV1",
+    "LykenoxAcousticMelPostnetCandidate",
 ]
